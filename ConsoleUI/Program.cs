521ef1df2b72a5988baeac379d5570548434999d
Yusuf Gok <gokyusuf41@hotmail.com>
2023-02-26 19:56:14 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,30 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using DataAccess.Concrete.InMemory;
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductManager productManager=new ProductManager(new EfProductDal());
+
+            //foreach (var product in productManager.GetAll())
+            //{
+            //    Console.WriteLine(product.ProductName);
+            //}
+
+            foreach (var product2 in productManager.GetAllByCategoryId(5))
+            {
+                Console.WriteLine(product2.ProductName);
+            }
+            Console.WriteLine("-------------------------------------");
+            foreach (var Product3 in productManager.GetByUnitPrice(10,25))
+            {
+                Console.WriteLine(Product3.ProductName);
+            }
+        }
+    }
+}
---
+++ DataAccess/Abstract/IEntityRepository.cs
@@ -0,0 +1,22 @@
+﻿using Entities.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccess.Abstract
+{
+    //generic constraint
+    //class:Referans Type
+    //IEntity: IEntity olabilir veya implemente eden bir nesne olabilir
+    //new(): new'lenebilir olmalı şartı koyar.
+    public interface IEntityRepository<T> where T : class,IEntity,new()
+    {
+        List<T> GetAll(Expression<Func<T, bool>> filter = null);
+        T Get(Expression<Func<T, bool>> filter);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+    }
+}
